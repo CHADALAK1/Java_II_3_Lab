aa7205771e719acc9c2bdc13a576546975645cbe
wit0230621 <chad_reddick2008@yahoo.com>
2015-09-24 10:44:52 -0500

    Started PizzaOrder class to extend from JFrame and to output a window

--- src/PizzaOrder.java
+++ src/PizzaOrder.java
@@ -1,11 +1,23 @@
 /**
  * Created by Chad Reddick on 9/24/2015.
  */
-public class PizzaOrder
+import javax.swing.*;
+
+
+public class PizzaOrder extends JFrame
 {
     public PizzaOrder()
     {
+        final int WINDOW_WIDTH = 350;
+        final int WINDOW_HEIGHT = 250;
 
+        setTitle("PIZZA");
+
+        setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setVisible(true);
     }
 
 }
--- src/main.java
+++ src/main.java
@@ -1,11 +1,14 @@
 /**
  * Created by Chad Reddick on 9/24/2015.
  */
+import javax.swing.*;
+
+
 public class main
 {
     public static void main(String[] args)
     {
-
+        PizzaOrder order = new PizzaOrder();
     }
 
 }
